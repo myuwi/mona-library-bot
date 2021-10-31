2c77fffa88ff5573be8f48d31db4c9c97cedef99
Myuwi <mikke012@gmail.com>
2021-10-31 15:36:59 +0200

    Removed presence

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -3,14 +3,4 @@ import { MClient } from '../client/MClient';
 
 export const event = async (client: MClient) => {
     console.log(chalk.cyan(`Logged in as ${client.user!.tag}`));
-
-    client.user!.setPresence({
-        status: 'online',
-        activities: [
-            {
-                type: 'PLAYING',
-                name: 'Mona Impact'
-            }
-        ]
-    });
 };
